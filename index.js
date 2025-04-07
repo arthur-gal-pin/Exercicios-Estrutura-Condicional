2addcff169b292ccf0e515bbfea7ec5ec15146a2
unknown <arthurrgspinto@gmail.com>
2025-04-07 14:21:58 -0300

    corrigido o código de par e ímpar

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const num = parseFloat(prompt("Insira um número:"));
+const num = parseInt(prompt("Insira um número:"));
 
 if (isNaN(num)) {
     alert('Erro: Você não digitou um número válido!')
 }
 
 if (!isNaN(num)); {
-    if (num > 0 && num != 0) {
-        alert("O número é positivo.");
-    } else if (num < 0 && num != 0) {
-        alert("O número é negativo.");
-    } else if (num == 0){
-        alert("O número é neutro.");
-    }
-}
+    if (num%2 == 0) {
+        alert("O número é par.");
+    } else if (num%2 != 0) {
+        alert("O número é ímpar.");
+    }}
