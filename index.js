a61860d7ff86581f6d82794982f316cd8ce0febc
unknown <arthurrgspinto@gmail.com>
2025-04-07 13:56:11 -0300

    Erros no código maioridade corrigidos

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const idade = parseInt(prompt("Informe sua idade:"));
+const idade = parseInt(prompt("Informe sua idade em anos:"));
 
 if (isNaN(idade)) {
     alert('Erro: Você não digitou uma idade válida!')
 }
 
 if (!isNaN(idade)); {
-    if (idade > 0) {
+    if (idade >= 0) {
         if (idade >= 18) {
             alert("Você é maior de idade!");
-        } else if (idade < 18 && idade != 0){
+        } else if (idade >= 0 && idade < 18) {
             alert("Você é menor de idade.");
         }
 
-    } else if (num < 0 && num != 0) {
+    } else if (idade < 0) {
         alert("A idade não pode ser negativa.");
-    } }
+    }
+}
