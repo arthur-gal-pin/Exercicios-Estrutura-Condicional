49becbc593eea7943c6aff1499290fa7ec4e053e
unknown <arthurrgspinto@gmail.com>
2025-04-07 13:28:57 -0300

    Criado o código de par e ímpar na branch par e impar

---
+++ index.js
@@ -0,0 +1,15 @@
+const num = parseInt(prompt("Insira um número:"));
+
+if (isNaN(num)) {
+    alert('Erro: Você não digitou um número válido!')
+}
+
+if (!isNaN(num)); {
+    if (num > 0 && num != 0) {
+        alert("O número é positivo.");
+    } else if (num < 0 && num != 0) {
+        alert("O número é negativo.");
+    } else if (num == 0){
+        alert("O número é neutro.");
+    }
+}
