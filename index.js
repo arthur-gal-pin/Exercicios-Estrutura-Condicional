b5b1cdb51eb2683ad70a99aec9795daf0c02a9f0
unknown <arthurrgspinto@gmail.com>
2025-04-07 16:03:01 -0300

    Comentários realizados.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const num = parseFloat(prompt("Insira um número:"));
 
+//Verifica se o input realizado pelo usuário no prompt é um número.
 if (isNaN(num)) {
     alert('Erro: Você não digitou um número válido!')
 }
 
 if (!isNaN(num)); {
-    if (num > 0 && num != 0) {
+    if (num > 0) //Verifica se o número é maior que zero, logo positivo.
+    {
         alert("O número é positivo.");
-    } else if (num < 0 && num != 0) {
+    } else if (num < 0 && num != 0)//Verifica se o número é menor que zero, logo negativo.
+    {
         alert("O número é negativo.");
-    } else if (num == 0){
+    } else if (num == 0) //Verifica se o número é zero, logo neutro.
+    {
         alert("O número é neutro.");
     }
 }
