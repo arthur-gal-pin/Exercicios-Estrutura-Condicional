1baf4a8998d2c6e365f1d7f61b7abe8852da742a
unknown <arthurrgspinto@gmail.com>
2025-04-07 14:36:36 -0300

    Criada a funcionalidade de desconto. Está funcional.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const num = parseFloat(prompt("Insira um número:"));
+const preco = parseFloat(prompt("Informe o preço da compra (apenas os números, separando decimal com ponto):"));
 
-if (isNaN(num)) {
-    alert('Erro: Você não digitou um número válido!')
+if (isNaN(preco)) {
+    alert('Erro: Você não digitou um valor válido!')
 }
 
-if (!isNaN(num)); {
-    if (num > 0 && num != 0) {
-        alert("O número é positivo.");
-    } else if (num < 0 && num != 0) {
-        alert("O número é negativo.");
-    } else if (num == 0){
-        alert("O número é neutro.");
+if (!isNaN(preco)); {
+    if (preco >= 100) {
+        alert(`Você recebeu um desconto de ${preco*0.1} reais! O valor total de sua compra será de, então, ${preco*0.9} reais`)
+    } else if (preco < 100 && preco > 0) {
+        alert(`A compra terá um valor de ${preco} reais`);
+    } else if (preco <= 0){
+        alert("A compra não pode custar nada, nem menos do que nada!");
     }
 }
