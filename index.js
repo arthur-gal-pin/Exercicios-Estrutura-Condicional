f9047ef06352cf6d572c5e31c94edf844769cee2
unknown <arthurrgspinto@gmail.com>
2025-04-07 15:53:29 -0300

    Comentários realizados.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const num = parseInt(prompt("Insira um número:"));
 
+//Verifica se o input realizado pelo usuário no prompt é um número.
 if (isNaN(num)) {
     alert('Erro: Você não digitou um número válido!')
 }
 
 if (!isNaN(num)); {
-    if (num%2 == 0) {
+    //Avalia se o número é maior ou igual a zero e divisível por 2 ao mesmo tempo.
+    if (num >= 0 && num % 2 == 0) {
         alert("O número é par.");
-    } else if (num%2 != 0) {
+    } else if (num >= 0 && num % 2 != 0) {
         alert("O número é ímpar.");
-    }}
+    } else if (num < 0) {
+        alert("Um número negativo não pode ser par, já que esse conceito é exclusivo ao conjunto dos números naturais.")
+    }
+}
