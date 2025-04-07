990050c8ccd500e7758de61146b8af525e1702dd
unknown <arthurrgspinto@gmail.com>
2025-04-07 14:50:17 -0300

    Criada a funcionalidade de divisibilidade por 5. Está funcional.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const preco = parseFloat(prompt("Informe o preço da compra (apenas os números, separando decimal com ponto):"));
+const num = parseInt(prompt("Informe um número:"));
 
-if (isNaN(preco)) {
+if (isNaN(num)) {
     alert('Erro: Você não digitou um valor válido!')
 }
 
-if (!isNaN(preco)); {
-    if (preco >= 100) {
-        alert(`Você recebeu um desconto de ${preco*0.1} reais! O valor total de sua compra será de, então, ${preco*0.9} reais`)
-    } else if (preco < 100 && preco > 0) {
-        alert(`A compra terá um valor de ${preco} reais`);
-    } else if (preco <= 0){
-        alert("A compra não pode custar nada, nem menos do que nada!");
+if (!isNaN(num)); {
+    if (num >= 0 && num%5 == 0) {
+        alert(`O número ${num} é divisível por 5.`)
+    } else if (num >= 0 && num%5 != 0) {
+        alert(`O número ${num} não é divisível por 5.`);
+    } else if (num <= 0){
+        alert("O número não pode ser negativo! O conceito de divisibilidade se aplica apenas ao conjunto dos números naturais.");
     }
 }
